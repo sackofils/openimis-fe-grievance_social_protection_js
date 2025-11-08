b72c30d3e30424487aeb7ca4fa55a3620ca317a4
sackofils <sackofils@gmail.com>
2025-11-08 16:01:43 +0000

    Désactivation du lien de creation d'une plainte.

--- src/pages/TicketsPage.js
+++ src/pages/TicketsPage.js
@@ -123,7 +123,7 @@ class TicketsPage extends Component {
               open={Boolean(anchorEl)}
               onClose={this.handleMenuClose}
             >
-              <MenuItem onClick={this.handleNative}>
+              <MenuItem onClick={this.handleNative} disabled={true}>
                 <DescriptionIcon className={classes.menuItemIcon} />
                 {formatMessage(intl, MODULE_NAME, "ticket.menu.createNative")}
               </MenuItem>
